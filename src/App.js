8e1af57f29bf8a1d2412d889d6db2f41ab5bab08
Renan Lage <renanlopresti@gmail.com>
2021-11-23 21:43:38 -0300

    adicionando o theme

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 import Axios from 'axios'
 import Header from './components/Header'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from './components/Theme';
+import CadastroPage from './components/NewJob'
 
 const DivAppContainer = styled.div`
   display: flex;
@@ -11,11 +14,14 @@ const DivAppContainer = styled.div`
 `
 
 function App() {
-	return (
-        <DivAppContainer>
-			<Header />
-		</DivAppContainer>
-	)
+  return (
+    <ThemeProvider theme={theme}>
+      <DivAppContainer>
+        <Header />
+        <CadastroPage/>
+      </DivAppContainer>
+    </ThemeProvider>
+  )
 }
 
 export default App
