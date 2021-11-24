4bc6d2d27e3cf44f2798c9577f5e343b49242ea5
Renan Lage <renanlopresti@gmail.com>
2021-11-23 22:52:11 -0300

    adicionando inputs

--- src/components/NewJob.js
+++ src/components/NewJob.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
+import styled from 'styled-components';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputAdornment from '@mui/material/InputAdornment';
+
+const CadastroContainer = styled.div`
+display: flex;
+flex-direction: column;
+
+`
+
 
 export default class CadastroPage extends React.Component {
-  render(){
-    return(
-      <div>
-        
-      </div>
+  render() {
+    return (
+      <CadastroContainer>
+        <h1>Cadastre o seu serviço</h1>
+        <TextField
+          id="outlined-basic"
+          label="Titulo"
+          variant="outlined"
+          margin="dense"
+        />
+        <TextField
+          id="outlined-basic"
+          label="Descrição"
+          variant="outlined"
+          margin="dense"
+        />
+        <FormControl fullWidth sx={{ m: 1 }}>
+          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            value={"ss"}
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="Amount"
+          />
+        </FormControl>
+      </CadastroContainer>
     )
   }
 }
